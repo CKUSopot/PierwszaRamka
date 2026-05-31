2c0bc41b4a8187e6650ce434ee5eb05fda35d0fa
Kostiantyn <56robik@gmail.com>
2026-05-31 09:56:37 +0200

    Wer. 002 - OK + opis

--- src/PierwszaRamka.java
+++ src/PierwszaRamka.java
@@ -3,17 +3,13 @@ import java.awt.*;
 
 public class PierwszaRamka {
     public static void main(String[] args) {
-        JFrame ramka = new JFrame("Tytuł okna");
-        ramka.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        ramka.setSize(400, 300);
-        //ramka.pack();//Ustawia minimalny
-        //rozmiar okna potrzebny
-        //do wyświetlenia
-        //wszystkich komponentów
-        //(wymaga Layout
-        //Managera)
-        ramka.setVisible(true);
-        ramka.setTitle("Moja pierwsza aplikacja desktopowa");
+        JFrame ramka = new JFrame("Tytuł okna");// 1. Tworzenie obiektu ramki (kontenera najwyższego poziomu)
+        ramka.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// 2. Ustawienie operacji zamykania ramki (ważne!)
+        // EXIT_ON_CLOSE: kończy działanie programu po zamknięciu okna
+        ramka.setSize(400, 300);// 3. Ustawienie rozmiaru okna, szerokość, wysokość w pikselach
+        //ramka.pack();//Ustawia minimalny rozmiar okna potrzebny do wyświetlenia wszystkich komponentów (wymaga Layout Managera)
+        ramka.setVisible(true);// 4. Ustawienie widoczności ramki
+        ramka.setTitle("Moja pierwsza aplikacja desktopowa");//tytuł okna
         //ramka.setLocation(500, 500);
         ramka.setLocationRelativeTo(null);//na środku ekranu
         //ramka.setResizable(false);
